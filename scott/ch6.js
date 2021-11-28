c0dc827b95bbf828e1429c8f7c4d42218f50206e
Scottyos <Scott.Akkermans@gmail.com>
2021-11-27 18:26:47 -0700

    Scott Coding Challenge 6 Complete

---
+++ scott/ch6.js
@@ -0,0 +1,25 @@
+const addAstronautToRoster = (roster, astronaut) => {
+    roster.push(astronaut)
+      return roster
+    }
+    
+  
+
+  const ex1 = []
+    const exampleAstronaut = {
+      firstName:"Chris",
+      lastName: "Hadfield",
+      nickname:"Space Oddity",
+      prefix:"Astronaut"
+    }
+
+console.log(addAstronautToRoster(ex1))
+
+
+
+// const addAstronautToRoster = (roster, astronaut) => {
+//     // Code here!
+  
+    
+//   }
+  
