b9936e623ad5c3b4519148897092392e29e3deb6
Scottyos <Scott.Akkermans@gmail.com>
2021-12-06 21:22:57 -0700

    Scott Coding Challenge 15 Updated

--- scott/ch15.js
+++ scott/ch15.js
@@ -1,5 +1,5 @@
 const launchDate = "2021-12-12"
-const fakeToday = "2021-12-01"
+//const fakeToday = "2021-12-01"
 // const fakeToday = new Date()
 const missionName = "Moon visit"
 
@@ -7,12 +7,15 @@ const missionName = "Moon visit"
 const timeRemaining = (launchDate, missionName, fakeToday) => {
 	const today = fakeToday || new Date()
     var date1 = new Date(launchDate)
-    var date2 = new Date(fakeToday)
+    var date2 = new Date(today)
     var timeDifference = date1.getTime() - date2.getTime()
-    var dayDifference = timeDifference / (1000 * 3600 * 24)
+    var dayDifference = Math.round(timeDifference / (1000 * 3600 * 24))
     const dateVariance = {daysRemaining: dayDifference, missionName: missionName}
      return dateVariance 
     }
 
-timeRemaining(launchDate, missionName, fakeToday)
-console.log(timeRemaining(launchDate, missionName, fakeToday))
+// timeRemaining(launchDate, missionName, fakeToday)
+// console.log(timeRemaining(launchDate, missionName, fakeToday))
+
+timeRemaining(launchDate, missionName)
+console.log(timeRemaining(launchDate, missionName))
